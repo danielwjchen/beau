b4caf14a8d7d785791bf62ec9633029dc06834b6
Daniel Chen <hello@danielwjchen.com>
2025-10-20 11:06:55 -0500

    Initial Commit

---
+++ BeauTests/BeauTests.swift
@@ -0,0 +1,16 @@
+//
+//  BeauTests.swift
+//  BeauTests
+//
+//  Created by Daniel Chen on 10/20/25.
+//
+
+import Testing
+
+struct BeauTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
